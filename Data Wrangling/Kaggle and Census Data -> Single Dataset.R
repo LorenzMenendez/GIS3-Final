1f302ad1716c61ccd1cf35b35e4f0fb4ad3eba2d
LorenzMenendez <lmenendez@uchicago.edu>
2019-05-19 17:02:29 -0500

    Wrangled Boundary and Demographic Data

---
+++ Data Wrangling/Kaggle and Census Data -> Single Dataset.R
@@ -0,0 +1,37 @@
+# Goal: Automatically download Kaggle Election Data from static URLs
+
+library(sf)
+library(dplyr)
+
+
+# STEP 1: Importing Data --------------------------------------------------
+
+
+# County Boundaries from Census Shapefile
+download.file("https://www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_us_county_500k.zip", "./DATA/countyBoundaries.zip")
+unzip("./DATA/countyBoundaries.zip", exdir = "./DATA")
+
+countyBoundaries16 = st_read("./DATA/cb_2014_us_county_500k.shp")
+
+countyBoundaries16$GEOID = as.numeric(levels(countyBoundaries16$GEOID))[countyBoundaries16$GEOID] # Required for join to work
+
+# County Quick Facts from Kaggle 
+# download.file("https://www.kaggle.com/benhamner/2016-us-election/downloads/county_facts.csv", "./DATA/countyQuickfacts.csv") # Broken and not working for some reason! 
+
+countyQuickfacts16 = read.csv("./DATA/countyQuickfacts.csv", sep = ",")
+
+
+# STEP 2: Wrangling into a single dataset ---------------------------------
+
+elctnData16 = inner_join(countyQuickfacts16, countyBoundaries16, by = c("fips" = "GEOID")) %>%
+        select(-c(STATEFP, COUNTYFP, COUNTYNS, AFFGEOID, NAME, LSAD, ALAND, AWATER)) %>%
+        st_sf() %>%
+        st_zm(drop = TRUE)
+
+
+# STEP 3: Sanity Checking by Plotting Data --------------------------------
+library(tmap)
+tmap_mode("view")
+tm_shape(elctnData16) + 
+        tm_borders("black", lwd = .5)
+
